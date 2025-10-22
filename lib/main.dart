40fa4a255de5a15bb7bd5bb7d60945e0b4b5c819
ankit060801 <ankitkumar975493@gmail.com>
2025-10-23 00:13:26 +0530

    committed

--- lib/main.dart
+++ lib/main.dart
@@ -215,7 +215,7 @@ class _EmployeeFormPageState extends State<EmployeeFormPage> {
               "Employee Data",
               style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
             ),
-            SizedBox(height: 12),
+            SizedBox(height: 10),
             // Employee List (Table)
             ListView.builder(
               shrinkWrap: true,
